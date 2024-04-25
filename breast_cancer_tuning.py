24c6dfb49038294ac38de42d44c1df9b7c1f7ab2
Gabriel Alves Baltazar <130671682+gabrielbaltazarmw2@users.noreply.github.com>
2024-04-25 18:47:28 -0300

    Add files via upload

---
+++ breast_cancer_tuning.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+previsores = pd.read_csv('entradas_breast.csv')
+classe = pd.read_csv('saidas_breast.csv')
+
+def criarRede(optimizer, loos, kernel_initializer, activation, neurons):
+    classificador = Sequential()
+    classificador.add(Dense(units = neurons, activation = activation, 
+                        kernel_initializer = kernel_initializer, input_dim = 30))
+    classificador.add(Dropout(0.2))
+    classificador.add(Dense(units = neurons, activation = activation, 
+                        kernel_initializer = kernel_initializer))
+    classificador.add(Dropout(0.2))
+    classificador.add(Dense(units = 1, activation = 'sigmoid'))
+    classificador.compile(optimizer = optimizer, loss = loos,
+                      metrics = ['binary_accuracy'])
+    return classificador
+
+classificador = KerasClassifier(build_fn = criarRede)
+parametros = {'batch_size': [10, 30],
+              'epochs': [50, 100],
+              'optimizer': ['adam', 'sgd'],
+              'loos': ['binary_crossentropy', 'hinge'],
+              'kernel_initializer': ['random_uniform', 'normal'],
+              'activation': ['relu', 'tanh'],
+              'neurons': [16, 8]}
+grid_search = GridSearchCV(estimator = classificador,
+                           param_grid = parametros,
+                           scoring = 'accuracy',
+                           cv = 5)
+grid_search = grid_search.fit(previsores, classe)
+melhores_parametros = grid_search.best_params_
+melhor_precisao = grid_search.best_score_
